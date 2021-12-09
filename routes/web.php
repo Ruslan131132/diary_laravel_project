270fc2d1504920409d09e1327bb71e5d9f8e9a30
Ruslan131132 <ruskhas131@mail.ru>
2021-12-09 18:40:58 +0300

    Написаны маршруты для аутентификации и добавлен namespace для user

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,17 @@ Route::get('/', function() {
     return view('pages.index');
 })->name('index');
 
+Route::get('/logout', function(){
+    Auth::logout();
+    return redirect(route('index'));
+})->name('logout');
+
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('login');
+
+Route::name('user.')->group(function(){
+    Route::view('/user',  'pages.user.user-main')->middleware('auth')->name('user-main');
+});
+
 Route::name('admin.')->group(function(){
     Route::view('/admin-main', 'pages.admin.admin_main')->middleware('auth')->middleware('admin')->name('admin-main');
     Route::view('/create-class',  'pages.admin.create_class')->middleware('auth')->middleware('admin')->name('create-class');
